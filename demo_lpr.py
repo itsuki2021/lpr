7a9cd805e81d1a548862c5d25595a23af291fca7
lst <shitong_liu@163.com>
2022-02-14 11:09:10 +0800

    整理putCnText()函数

--- demo_lpr.py
+++ demo_lpr.py
@@ -4,16 +4,13 @@ from hyperlpr import *
 from PIL import Image, ImageDraw, ImageFont
 
 
-def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
-
+def putCnText(img, text, org, textColor=(0, 255, 0), textSize=20):
     if isinstance(img, numpy.ndarray):
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
     draw = ImageDraw.Draw(img)
-    fontText = ImageFont.truetype(
-        "font/simsun.ttc", textSize, encoding="utf-8")
-
-    draw.text((left, top), text, textColor, font=fontText)
+    font = ImageFont.truetype("font/simsun.ttc", textSize, encoding="utf-8")
+    draw.text(xy=org, text=text, fill=textColor, font=font)
     return cv2.cvtColor(numpy.asarray(img), cv2.COLOR_RGB2BGR)
 
 
@@ -23,21 +20,20 @@ if __name__ == '__main__':
 
     os.makedirs(output_prefix, exist_ok=True)
     names = os.listdir(input_prefix)
+    names.sort()
 
     for name in names:
         input_path = os.path.join(input_prefix, name)
         output_path = os.path.join(output_prefix, name)
         img = cv2.imread(input_path)
+        if img is None:
+            continue
+
         result = HyperLPR_plate_recognition(img)
         for i in range(len(result)):
             txt, conf, bbox = result[i]
-            cv2.rectangle(img,
-                          pt1=bbox[:2],
-                          pt2=bbox[2:],
-                          color=(0, 0, 255),
-                          thickness=1,
-                          lineType=cv2.LINE_AA)
-            img = cv2ImgAddText(img, txt, bbox[0], bbox[1] - 30, (255, 0, 0), 30)
+            cv2.rectangle(img, pt1=bbox[:2], pt2=bbox[2:], color=(0, 0, 255), thickness=1, lineType=cv2.LINE_AA)
+            img = putCnText(img, txt, (bbox[0], bbox[1] - 30), (255, 0, 0), 30)
         cv2.imwrite(output_path, img)
         print(f"{output_path}\n\n")
 
