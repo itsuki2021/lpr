0a50005b600791d35d09fbb613165a8d834657ee
lst <shitong_liu@163.com>
2022-03-01 11:30:11 +0800

    修改快照demo

--- demo_get_snap.py
+++ demo_get_snap.py
@@ -1,6 +1,7 @@
 from http.client import HTTPConnection
 from PIL import Image
 from argparse import ArgumentParser
+from loguru import logger
 import numpy as np
 import json
 import hashlib
@@ -8,7 +9,6 @@ import time
 import cv2
 import io
 import sys
-import yaml
 
 
 def encrypt(s: str) -> str:
@@ -24,58 +24,57 @@ def encrypt(s: str) -> str:
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--config', type=str, default='config/config.yaml', help='yaml config file')
-    opt = parser.parse_args()
-
+    parser.add_argument('--host', type=str, default='8.141.69.166', help='host address, e.g. 0.0.0.0')
+    parser.add_argument('--port', type=int, default=10256, help='host port e.g. 8000')
+    parser.add_argument('--usr', type=str, default='admin', help='user name')
+    parser.add_argument('--pwd', type=str, default='admin', help='password')
+    parser.add_argument('--serial', type=str, default='34020000001110000031', help='LiveGBS device serial id')
+    parser.add_argument('--channel', type=int, default=1, help='LiveGBS stream channel')
+    parser.add_argument('--num_test', type=int, default=10, help='number of test')
     # 读取参数
-    try:
-        with open(opt.config, 'r', encoding='utf-8') as f:
-            cfg = yaml.safe_load(f)
-    except Exception as e:
-        print(f'Can not found {opt.config}\n\t{e}')
-        sys.exit(-1)
+    opt = parser.parse_args()
 
     # 登录LiveGBS，获取token
     url_login = f'/api/v1/login?' \
-                f'username={cfg["usr"]}&password={encrypt(cfg["pwd"])}'
+                f'username={opt.user}&password={encrypt(opt.pwd)}'
     headers = {'Content-Type': 'text/html;charset=utf-8'}
     try:
-        conn = HTTPConnection(host=cfg["host"], port=cfg["port"])
-        conn.request(method=cfg["method"], url=url_login, headers=headers)
+        conn = HTTPConnection(host=opt.host, port=opt.port)
+        conn.request(method="GET", url=url_login, headers=headers)
         rsp = conn.getresponse()
-        print('Login')
-        print('\tResponse.status' + str(rsp.status))
-        print('\tRespond.reason' + str(rsp.reason))
+        logger.info('Login')
+        logger.info('\tResponse.status' + str(rsp.status))
+        logger.info('\tRespond.reason' + str(rsp.reason))
         bts = rsp.read()
         data = json.loads(str(bts, 'utf-8'))
         token = data['URLToken']
     except Exception as e:
-        print(f'\nFailed to login\n\t{e}')
+        logger.info(f'\nFailed to login\n\t{e}')
         sys.exit(-1)
 
     # 请求实时快照
     url_snap = f'/api/v1/device/channelsnap?' \
                f'token={token}' \
-               f'&serial={cfg["serial"]}' \
-               f'&channel={cfg["channel"]}' \
+               f'&serial={opt.serial}' \
+               f'&channel={opt.channel}' \
                f'&realtime=true'
     try:
-        for i in range(cfg["num_test"]):
+        for i in range(opt.num_test):
             tic = time.time()
-            conn.request(method=cfg["method"], url=url_snap)
+            conn.request(method="GET", url=url_snap)
             rsp = conn.getresponse()
-            print(f'\nSnap {i}')
-            print('\tResponse.status' + str(rsp.status))
-            print('\tRespond.reason' + str(rsp.reason))
+            logger.info(f'\nSnap {i}')
+            logger.info('\tResponse.status' + str(rsp.status))
+            logger.info('\tRespond.reason' + str(rsp.reason))
             bts = rsp.read()
             img = np.asarray(Image.open(io.BytesIO(bts)))
             conn.close()
 
             cv2.namedWindow("image", cv2.WINDOW_NORMAL)
             cv2.imshow("image", img)
-            cv2.waitKey(2)
+            cv2.waitKey(1)
     except Exception as e:
-        print(f'\n!!! error !!!\n\t{e}')
+        logger.error(e)
         sys.exit(-1)
 
-    print("Done.")
+    logger.info("Done.")
